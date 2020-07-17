51c10042d067db7d33e3f4e95b673c0a3e294671
Jacob <jacob.reilly.cooper@gmail.com>
2020-07-17 13:06:15 +0100

    query string for city added.

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -2,8 +2,8 @@
 const weather = require('../lib/weather');
 
 exports.getIndex = async (req, res) => {
-    let city = "Manchester"
+    let city = req.query.city;
     let data = await weather(city);
     console.log(data)
-    res.send('hello')
+    res.send(data);
 }
