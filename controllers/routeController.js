2893b5a9738ab818d98443a6a471ae34201583bc
Jacob <jacob.reilly.cooper@gmail.com>
2020-07-17 12:59:38 +0100

    data for weather created.

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -2,5 +2,8 @@
 const weather = require('../lib/weather');
 
 exports.getIndex = async (req, res) => {
+    let city = "Manchester"
+    let data = await weather(city);
+    console.log(data)
     res.send('hello')
 }
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 // internal imports
 const routes = require('./routes');
